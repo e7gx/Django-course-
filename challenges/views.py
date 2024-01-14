1cf1cfd93b9e04110bfbefcd56c48dc7ed5df978
e7gx <abalgabo@gmail.com>
2024-01-14 04:39:47 +0300

    .

--- challenges/views.py
+++ challenges/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('hi abdullah ,  you can know switch between pages \n Good for you')
+    return HttpResponse('hi abdullah ,  you can know switch between pages')
+
