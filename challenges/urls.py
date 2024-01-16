4d26a9e682142c064940dc4e831010d406652b90
e7gx <abalgabo@gmail.com>
2024-01-16 21:25:58 +0300

    switch between urls

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("jan",views.index)
+    path("<pages>", views.pages),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse('hi abdullah ,  you can know switch between pages')
+def step1(request):
+    return HttpResponse('hi 1 ,  you can know switch between pages')
 
+
+def step2(request):
+    return HttpResponse('hi 2')
+
+
+def step3(request):
+    return HttpResponse('hi 3 ,  you can know switch between pages')
+
+
+def step4(request):
+    return HttpResponse('hi 4')
+
+
+
+def pages(request,pages):
+    pages_user = None
+    if pages == "abdullah":
+        pages_user = "ارحب مليون"
+    elif pages == "ibrahim":    
+          pages_user = " 2 ي هلا ومرحبا"
+    elif pages == "mo":    
+          pages_user = "3 ي هلا ومرحبا"
+    elif pages == "gh":    
+          pages_user = "4 ي هلا ومرحبا"
+    else :
+         return HttpResponseNotFound('الخلا')
+    return HttpResponse(pages_user)
