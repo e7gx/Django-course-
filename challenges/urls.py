9eba4cad3c33048e0c3eaebbeb08818cf2177d5d
e7gx <abalgabo@gmail.com>
2024-01-23 00:15:15 +0300

    //

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index),
+    path("",views.index,name= "index"),
     path("<int:page>", views.pagesByNumbers),
     path("<str:page>", views.page, name="abdullah-test"),
 ]
